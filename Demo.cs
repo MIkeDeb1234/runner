232127a399a55cc4e41395f3b1b096ed7d08cc67
Mike <Mike@gmail.com>
2024-07-04 22:43:05 +0300

    chore: Add initial project files and code

---
+++ Demo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace runner
+{
+    public class Demo
+    {
+        public static void Load()
+        {
+            Console.WriteLine("Hello World!");
+            File.WriteAllText("test.txt", "Hello World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using runner;
+
+namespace Runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo.Load();
+
+        }
+    }
+}
